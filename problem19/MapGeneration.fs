095a9578130cbfbef9dfe9184ef8a8391ba2b128
arahnik <abdujalol04@mail.ru>
2025-02-13 02:11:58 +0300

    some fixing

--- problem19/MapGeneration.fs
+++ problem19/MapGeneration.fs
@@ -3,27 +3,22 @@ module Lab1.Problem19.MapGen
 let daysInMonth = [| 31; 28; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31 |]
 
 let isLeapYear year =
-    year % 4 = 0 && year % 100 <> 0 || year % 400 = 0
+    (year % 4 = 0 && year % 100 <> 0) || (year % 400 = 0)
 
 let dates =
     [ for y in 1901..2000 do
           for m in 0..11 -> y, m ]
 
-let rec calculateFirstDay (year, month) =
-    let rec daysInYear y =
+let calculateFirstDay (year, month) =
+    let daysInYear y =
         [ for m in 0..11 -> if m = 1 && isLeapYear y then 29 else daysInMonth.[m] ]
 
-    let rec daysUpToYear y =
-        match y with
-        | 1901 -> 0
-        | _ -> (daysInYear (y - 1) |> List.sum) + daysUpToYear (y - 1)
+    let daysUpToYear y =
+        seq { 1901 .. y - 1 }
+        |> Seq.map (fun yr -> daysInYear yr |> List.sum)
+        |> Seq.sum
 
-    let rec daysUpToMonth y m =
-        match m with
-        | 0 -> 0
-        | _ ->
-            (if m = 1 && isLeapYear y then 29 else daysInMonth.[m - 1])
-            + daysUpToMonth y (m - 1)
+    let daysUpToMonth y m = daysInYear y |> List.take m |> List.sum
 
     let totalDays = daysUpToYear year + daysUpToMonth year month
     (totalDays + 2) % 7
